b010714f40481014c321ec4bff34bad916cc99f3
26fati <fatibeautiful1@gmail.com>
2024-03-21 23:18:01 +0100

    more tasks

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,6 +6,6 @@ CREATE TRIGGER reset_valid_email AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email THEN
-        UPDATE users SET NEW.valid_email = 0;
+        UPDATE users SET NEW.valid_email = 0
     END IF;
 END;
