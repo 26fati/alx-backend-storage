a93836288a35ba567418e6d336d835901a97f6b2
26fati <fatibeautiful1@gmail.com>
2024-03-25 21:46:05 +0100

    task 102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -9,30 +9,31 @@ from pymongo import MongoClient
 from bson.son import SON
 
 
-client = MongoClient("localhost", 27017)
+if __name__ == "__main__":
+    client = MongoClient("localhost", 27017)
 
-db = client.logs
-nginx = db.nginx
+    db = client.logs
+    nginx = db.nginx
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-logs = nginx.count_documents({})
-print("{} logs".format(logs))
-print("Methods:")
-for method in methods:
-    number = nginx.count_documents({"method": method})
-    print("\tmethod {}: {}".format(method, number))
-number_status = nginx.count_documents(
-    {"method": 'GET', "path": {"$regex": "^/status"}})
-print("{} status check".format(number_status))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    logs = nginx.count_documents({})
+    print("{} logs".format(logs))
+    print("Methods:")
+    for method in methods:
+        number = nginx.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, number))
+    number_status = nginx.count_documents(
+        {"method": 'GET', "path": {"$regex": "^/status"}})
+    print("{} status check".format(number_status))
 
-results = nginx.aggregate([
-        {"$group": {
-            "_id": "$ip",
-            "sum": {"$sum": 1}
-        }},
-        {"$sort": SON([("sum", -1)])}
-    ])
-print("IPS:")
-lst = list(results)
-for i in range(10):
-    print("\t{}: {}".format(lst[i].get("_id"), lst[i].get("sum")))
+    results = nginx.aggregate([
+            {"$group": {
+                "_id": "$ip",
+                "sum": {"$sum": 1}
+            }},
+            {"$sort": SON([("sum", -1)])}
+        ])
+    print("IPS:")
+    lst = list(results)
+    for i in range(10):
+        print("\t{}: {}".format(lst[i].get("_id"), lst[i].get("sum")))
